6c0af54b847febacb4bc8b0bff8e1c7d3e364982
Yilun Ying <yyilun@yahoo.com.sg>
2017-11-29 13:27:25 -0500

    Update SudokuBox.java

--- SudokuBox.java
+++ SudokuBox.java
@@ -2,6 +2,8 @@ import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.Queue;
 
+// This is the SudokuBox object that contain the actual value and possible values for each grid box. 
+//This object will be instantiated 9x9 times when the program runs.
 
 public class SudokuBox {
 	public Integer value;
